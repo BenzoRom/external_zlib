4944566a86360d7df1c8136d29543cb9eec6eb9f
Jun <ryan.j.he@gmail.com>
2017-04-20 15:54:21 +0800

    fill_window_arm: Neon-Optimized hash chain rebase
    Change-Id: I284700c788b02e4742b4b1e2377556a2a41dc556
    Signed-off-by: Jun He <jun.he@arm.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- contrib/optimizations/fill_window_arm.h
+++ contrib/optimizations/fill_window_arm.h
@@ -1,8 +1,9 @@
-/* fill_window_arm.c -- Optimized hash table shifting for ARM
+/* fill_window_arm.c -- Optimized hash table shifting for ARM with support for NEON instructions
  * Copyright (C) 2017 Mika T. Lindqvist
  *
  * Authors:
  * Mika T. Lindqvist <postmaster@raasu.org>
+ * Jun He <jun.he@arm.com>
  *
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
@@ -19,6 +20,44 @@
 
 extern unsigned ZLIB_INTERNAL deflate_read_buf OF((z_streamp strm, unsigned char *buf, unsigned size));
 
+#if (defined(__ARM_NEON__) || defined(__ARM_NEON))
+#include <arm_neon.h>
+
+/* SIMD version of hash_chain rebase */
+static inline void slide_hash_chain(Pos *table, unsigned int entries, uint16_t window_size) {
+    register uint16x8_t v, *p;
+    register size_t n;
+
+    size_t size = entries*sizeof(table[0]);
+    Assert((size % sizeof(uint16x8_t) * 8 == 0), "hash table size err");
+
+    Assert(sizeof(Pos) == 2, "Wrong Pos size");
+    v = vdupq_n_u16(window_size);
+
+    p = (uint16x8_t *)table;
+    n = size / (sizeof(uint16x8_t) * 8);
+    do {
+        p[0] = vqsubq_u16(p[0], v);
+        p[1] = vqsubq_u16(p[1], v);
+        p[2] = vqsubq_u16(p[2], v);
+        p[3] = vqsubq_u16(p[3], v);
+        p[4] = vqsubq_u16(p[4], v);
+        p[5] = vqsubq_u16(p[5], v);
+        p[6] = vqsubq_u16(p[6], v);
+        p[7] = vqsubq_u16(p[7], v);
+        p += 8;
+    } while (--n);
+}
+#else
+/* generic version for hash rebase */
+static inline void slide_hash_chain(Pos *table, unsigned int entries, uint16_t window_size) {
+    unsigned int i;
+    for (i = 0; i < entries; i++) {
+        table[i] = (table[i] >= window_size) ? (table[i] - window_size) : NIL;
+    }
+}
+#endif
+
 void fill_window_arm(deflate_state *s) {
     register unsigned n;
     unsigned long more;  /* Amount of free space at the end of the window. */
@@ -33,8 +72,6 @@ void fill_window_arm(deflate_state *s) {
          * move the upper half to the lower one to make room in the upper half.
          */
         if (s->strstart >= wsize+MAX_DIST(s)) {
-            unsigned int i;
-
             memcpy(s->window, s->window+wsize, wsize);
             s->match_start -= wsize;
             s->strstart    -= wsize; /* we now have strstart >= MAX_DIST */
@@ -46,24 +83,9 @@ void fill_window_arm(deflate_state *s) {
                later. (Using level 0 permanently is not an optimal usage of
                zlib, so we don't care about this pathological case.)
              */
-            {
-                n = s->hash_size;
-                for (i = 0; i < n; i++) {
-                    if (s->head[i] >= wsize)
-                        s->head[i] -= wsize;
-                    else
-                        s->head[i] = NIL;
-                }
-            }
 
-            {
-                for (i = 0; i < wsize; i++) {
-                    if (s->prev[i] >= wsize)
-                        s->prev[i] -= wsize;
-                    else
-                        s->prev[i] = NIL;
-                }
-            }
+            slide_hash_chain(s->head, s->hash_size, wsize);
+            slide_hash_chain(s->prev, wsize, wsize);
             more += wsize;
         }
         if (s->strm->avail_in == 0)
